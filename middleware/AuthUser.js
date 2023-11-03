e092f005c3583c94921af2bc45e629b4dbcd903c
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-04 05:17:50 +0700

    [msg] added message into code

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -1,33 +1,44 @@
 import User from "../models/UserModel.js";
 
+// mengeksport variabe supaya bisa digunakan di file lain
 export const verifyUser = async (req, res, next) => {
-  if (!req.session.userId) {
-    return res.status(401).json({ message: "Mohon login terlebih dahulu" });
-  }
 
+  // jika user belum login
+  if (!req.session.userId) return res.status(401).json({ message: "Mohon login terlebih dahulu" });
+
+  // jika user sudah login
   const user = await User.findOne({
     where: {
       uuid: req.session.userId,
     },
   });
 
+  // jika data user tidak ditemukan
   if (!user) return res.status(404).json({ message: "User tidak ditemukan" });
 
   req.userId = user.id;
   req.role = user.role;
+
+  // mengizinkan untuk melanjutkan
   next();
 };
 
+// mengeksport variabe supaya bisa digunakan di file lain
 export const adminOnly = async (req, res, next) => {
+
+  // mencari user sesuai dengan id
   const user = await User.findOne({
     where: {
       uuid: req.session.userId,
     },
   });
 
+  // jika user tidak ditemukan
   if (!user) return res.status(404).json({ message: "User tidak ditemukan" });
   
-  if (user.role !== "admin")
-    return res.status(403).json({ message: "Akses terlarang" });
+  // jika user bukan admin
+  if (user.role !== "admin") return res.status(403).json({ message: "Akses terlarang" });
+  
+  //mengizinkan untuk melanjutkan
   next();
 };
